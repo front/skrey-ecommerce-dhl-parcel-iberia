baa2a94f3df66e33a96f8fff24d22e6b50259e18
Sofia Sousa <csofiamsousa@gmail.com>
2021-03-09 17:30:49 +0000

    fix: Remove unset variable from function call

--- dependencies/dhl_ws_library/ParcelShopTracker.php
+++ dependencies/dhl_ws_library/ParcelShopTracker.php
@@ -1,20 +1,20 @@
 <?php
 
 class ParcelShopTracker extends DHLWS{
-    
+
     const PATH = '/track-trace';
 
     public function __construct (AuthDTO $authDTO, $updateTokenCallback = false ) {
         parent::__construct($authDTO, $updateTokenCallback);
     }
-   
+
     public function getTrackAndTrace(TrackAndTraceDTO $dto){
         try {
             $curl = $this->buildCurl(self::PATH, CURLOPT_HTTPGET,'?'.http_build_query($dto->toArray()) );
             $result = curl_exec($curl);
 
             if(curl_getinfo($curl, CURLINFO_HTTP_CODE)==401 || curl_getinfo($curl, CURLINFO_HTTP_CODE) ==403){
-               $result = $this->handleUnauthorized($curl, $headers);
+               $result = $this->handleUnauthorized($curl);
             }
 
         }catch(Exception $e){
